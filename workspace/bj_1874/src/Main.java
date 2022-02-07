4d650e14451cd936de664bacca68fccebe34b379
DaaEun <didekdms123@gmail.com>
2022-02-08 01:18:02 +0900

    220207 doing bj_1874

--- workspace/bj_1874/src/Main.java
+++ workspace/bj_1874/src/Main.java
@@ -1,4 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+// 스택 수열
 public class Main {
 
+	static StringBuffer sb = new StringBuffer();
+	static Stack<Integer> stack = new Stack<Integer>();
+	static int[] input;
+	static int n;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		input = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+		isProgressed(0, 1);
+		System.out.println(sb.toString());
+	}
+
+	private static void isProgressed(int idx, int num) {	// idx: input 값을 가져오기 위한 인덱스  n: stack에 담을 수
+
+		if(num == n && stack.empty()) return;
+		else {
+			sb.delete(0, sb.length());
+			sb.append("NO");
+			return;
+		}
+
+		if(stack.empty() || stack.peek() != input[idx]) {
+			stack.push(num);
+			sb.append("+\n");
+			isProgressed(idx, ++num);
+		} else if(stack.peek() == input[idx]) {	// stack.peek() == input[idx]
+			stack.pop();
+			sb.append("-\n");
+			isProgressed(++idx, num);
+		}
+	}
 }

2fb28011203992f6ac3292a89b2dcb32e12afc1c
DaaEun <didekdms123@gmail.com>
2022-02-19 23:28:13 +0900

    220219 solve bj

---
+++ workspace/bj_15650/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int N, M;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		
+	}
+}

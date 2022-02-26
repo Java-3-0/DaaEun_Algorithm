3ada3aa5fcbc595ad0b201ebe890f98cae39f1eb
DaaEun <didekdms123@gmail.com>
2022-02-26 23:50:15 +0900

    220225 doing bj

---
+++ workspace/bj_13300/src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Student implements Comparable<Student>{
+		int sex;
+		int year;
+		
+		public Student(int sex, int year) {
+			super();
+			this.sex = sex;
+			this.year = year;
+		}
+
+		@Override
+		public int compareTo(Student o) {
+			return this.year != o.year ? this.year - o.year : this.sex - o.sex;
+		}
+	}
+	
+	static int N, K, roomCnt;
+	static Student[] students;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		students = new Student[N];
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			students[i] = new Student(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
+		
+		int y = 0;
+		int s = 0;
+		for(Student student : students) {
+			if()
+		}
+		
+	}
+}

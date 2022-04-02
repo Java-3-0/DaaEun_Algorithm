1b700e403e2c5478d1ddfa43b217d7daf6795808
DaaEun <didekdms123@gmail.com>
2022-04-02 12:17:41 +0900

    0402 solve bj

--- workspace/baekjoon/src/치즈_2636_Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class 치즈_2636_Main {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int R = Integer.parseInt(br.readLine())
-	}
-}

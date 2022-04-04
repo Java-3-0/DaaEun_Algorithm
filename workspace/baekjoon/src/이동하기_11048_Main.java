b7767578898e40a16c0aeb262597fca6f73d6de4
DaaEun <didekdms123@gmail.com>
2022-04-04 22:24:20 +0900

    0404 solve bj

--- workspace/baekjoon/src/이동하기_11048_Main.java
+++ workspace/baekjoon/src/이동하기_11048_Main.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 이동하기_11048_Main {
-	public static void main(String[] args) {
+	
+	static int[] dr = {1, 0, 1};
+	static int[] dc = {0, 1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
+		int[][] maze = new int[N][M];
+		for (int r = 0; r < N; r++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int c = 0; c < M; c++) {
+				maze[r][c] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] candy = new int[N][M];
+		candy[0][0] = maze[0][0];
+		int nr = 0, nc = 0;
+		while(nr<N && nc<M) {
+			
+		}
+		
+		System.out.println(candy[N-1][M-1]);
 	}
 }

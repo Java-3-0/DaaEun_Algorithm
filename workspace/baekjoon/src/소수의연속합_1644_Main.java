729d777b976349c17ad29bb85c06c2671bdc947d
DaaEun <didekdms123@gmail.com>
2022-05-18 23:00:00 +0900

    baekjoon 0518

--- workspace/baekjoon/src/소수의연속합_1644_Main.java
+++ workspace/baekjoon/src/소수의연속합_1644_Main.java
@@ -1,4 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 소수의연속합_1644_Main {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringTokenizer st = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+	}
 }

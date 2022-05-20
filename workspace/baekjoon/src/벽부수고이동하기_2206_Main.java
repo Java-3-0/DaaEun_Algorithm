ae85208085b077704ab2040fb8f9a2e2a3f5ed84
DaaEun <didekdms123@gmail.com>
2022-05-20 23:00:00 +0900

    baekjoon 0520

--- workspace/baekjoon/src/벽부수고이동하기_2206_Main.java
+++ workspace/baekjoon/src/벽부수고이동하기_2206_Main.java
@@ -1,4 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 벽부수고이동하기_2206_Main {
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringTokenizer st = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+	}
 }
